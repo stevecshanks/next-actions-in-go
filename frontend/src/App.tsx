f0027e6b90201d870d99d8a1cae59dd30d801f14
Steve Shanks <steve.c.shanks@gmail.com>
2019-10-25 16:21:16 +0100

    Add basic frontend to pull from API

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface Action {
+  id: string;
+  name: string;
+}
+
+const App: React.FC = () => {
+  const [actions, setActions] = useState<Action[]>([]);
+
+  const fetchActions = () => {
+    fetch("/actions")
+      .then(response => response.json())
+      .then(json => setActions(json.data))
+      .catch(error => console.log("An error occurred:", error));
+  };
+
+  useEffect(fetchActions, []);
+
+  return (
+    <div>
+      <h1>Next Actions</h1>
+      <ul>
+        {actions.map((action: Action) => (
+          <li key={action.id}>{action.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
