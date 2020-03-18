0fe121a3103b1ab3ea2906c89ba39357263d901c
Steve Shanks <steve.c.shanks@gmail.com>
2020-03-18 19:10:25 +0000

    Add basic end-to-end test
    Create React App still uses an old version of jsdom, hence the update to
    use jest-environment-jsdom-sixteen

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import fetchMock from "jest-fetch-mock";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("renders the actions returned by the API", async () => {
+  const response = {
+    data: [
+      {
+        type: "actions",
+        id: "12345",
+        name: "An action"
+      }
+    ]
+  };
+  fetchMock.mockResponse(JSON.stringify(response));
+
+  const { findByText } = render(<App />);
+
+  const action = await findByText("An action");
+  expect(action).toBeInTheDocument();
 });
