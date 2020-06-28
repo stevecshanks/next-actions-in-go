5cf692806133c88d4ebfc1da25b48c7d4586744d
Steve Shanks <steve.c.shanks@gmail.com>
2020-06-28 11:02:22 +0100

    Extract out expected responses to files

--- api/cmd/api/api.go
+++ api/cmd/api/api.go
@@ -25,6 +25,7 @@ func handleError(w http.ResponseWriter, err error) {
 	if err != nil {
 		panic(err)
 	}
+	body = append(body, "\n"...)
 
 	w.WriteHeader(http.StatusInternalServerError)
 	_, err = w.Write(body)
