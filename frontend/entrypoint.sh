e6e8f4575b8988be733c696c5fe2037e1ec9921f
Steve Shanks <steve.c.shanks@gmail.com>
2020-08-01 15:33:19 +0100

    Add cron
    This wasn't previously installed, so the certbot was not running
    automatically.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,5 +4,7 @@ envsubst '$$SERVER_NAME' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/
 
 certbot --nginx -n --email ${EMAIL} --agree-tos --no-eff-email -d ${SERVER_NAME}
 
+cron
+
 # certbot will run nginx in background, so prevent container exiting as soon as it starts
 tail -f /dev/null
